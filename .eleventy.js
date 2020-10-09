270dc07afa83ffdb8262462ca6ddb07ea0745bc5
Daniel Zappala <daniel.zappala@gmail.com>
2020-10-08 21:03:16 -0600

    basic setup with search

--- .eleventy.js
+++ .eleventy.js
@@ -5,123 +5,127 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const squash = require("./_filters/squash");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.addPlugin(pluginNavigation);
 
-  eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+    eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
-
-    return array.slice(0, n);
-  });
-
-  eleventyConfig.addCollection("tagList", function(collection) {
-    let tagSet = new Set();
-    collection.getAll().forEach(function(item) {
-      if( "tags" in item.data ) {
-        let tags = item.data.tags;
-
-        tags = tags.filter(function(item) {
-          switch(item) {
-            // this list should match the `filter` list in tags.njk
-            case "all":
-            case "nav":
-            case "post":
-            case "posts":
-              return false;
-          }
-
-          return true;
-        });
-
-        for (const tag of tags) {
-          tagSet.add(tag);
-        }
-      }
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
     });
 
-    // returning an array in addCollection works in Eleventy 0.5.3
-    return [...tagSet];
-  });
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+    // Add a filter using the Config API
+    eleventyConfig.addFilter("squash", squash);
 
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (n < 0) {
+            return array.slice(n);
+        }
 
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+        return array.slice(0, n);
+    });
 
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
+    eleventyConfig.addCollection("tagList", function(collection) {
+        let tagSet = new Set();
+        collection.getAll().forEach(function(item) {
+            if ("tags" in item.data) {
+                let tags = item.data.tags;
+
+                tags = tags.filter(function(item) {
+                    switch (item) {
+                        // this list should match the `filter` list in tags.njk
+                        case "all":
+                        case "nav":
+                        case "post":
+                        case "posts":
+                            return false;
+                    }
+
+                    return true;
+                });
+
+                for (const tag of tags) {
+                    tagSet.add(tag);
+                }
+            }
         });
-      },
-    },
-    ui: false,
-    ghostMode: false
-  });
 
-  return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+        // returning an array in addCollection works in Eleventy 0.5.3
+        return [...tagSet];
+    });
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
+    eleventyConfig.addPassthroughCopy("img");
+    eleventyConfig.addPassthroughCopy("css");
 
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+    /* Markdown Overrides */
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAnchor, {
+        permalink: true,
+        permalinkClass: "direct-link",
+        permalinkSymbol: "#"
+    });
+    eleventyConfig.setLibrary("md", markdownLibrary);
 
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
+    // Browsersync Overrides
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function(err, browserSync) {
+                const content_404 = fs.readFileSync('_site/404.html');
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+                browserSync.addMiddleware("*", (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            },
+        },
+        ui: false,
+        ghostMode: false
+    });
 
-    // These are all optional, defaults are shown:
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+            "liquid"
+        ],
+
+        // If your site lives in a different subdirectory, change this.
+        // Leading or trailing slashes are all normalized away, so don’t worry about those.
+
+        // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+        // This is only used for link URLs (it does not affect your file structure)
+        // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+        // You can also pass this in on the command line using `--pathprefix`
+        // pathPrefix: "/",
+
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+
+        // These are all optional, defaults are shown:
+        dir: {
+            input: ".",
+            includes: "_includes",
+            data: "_data",
+            output: "_site"
+        }
+    };
 };
---
+++ posts/mom's-secret-chocolate-chip-cake-recipe.md
@@ -0,0 +1,60 @@
+---
+title: Mom's Secret Chocolate Chip Cake Recipe
+ate: 2014-04-06
+tags:
+    - dessert
+image: /img/chocolate-chip-cake.jpg
+layout: layouts/post.njk
+---
+
+
+My mom has always made this chocolate chip cake for our birthdays and
+holidays. It is so good, and we’ve never seen anyone else make it, so
+we took to calling it her "secret" recipe. Thankfully, she shared it
+with us and other family members so that we can pass on this amazing
+tradition. Thanks, mom!
+
+![chocolate chip cake](/img/chocolate-chip-cake.jpg)
+
+## Batter
+
+Blend:
+
+* 1 cube butter, softened
+* 1 cup sugar
+
+Beat in
+
+* 2 eggs
+
+Mix together in a separate bowl:
+
+* 2 cups flour
+* 1/2 teaspoons salt
+* 1/2 teaspoon baking soda
+* 1 teaspoon baking powder
+
+Alternately add the dry ingredients and:
+
+* 1 cup sour cream
+
+## Cake Assembly
+
+Mix together:
+
+* 1/4 cup sugar
+* 1/2 teaspoon cinnamon
+
+Spray an angel food cake pan. Alternately add to the pan:
+
+* 1/2 cake batter
+* 1/2 bag chocolate chips
+* 1/2 cinnamon/sugar mixture
+* rest of batter
+* rest of chocolate chips
+* rest of cinnamon/sugar mixture
+
+You’ll need to spread the batter with a spatula, because it is
+thick. It will look like you don’t have enough batter, but be patient.
+
+Bake at 325° for 40 – 45 minutes. Let cool before eating.
